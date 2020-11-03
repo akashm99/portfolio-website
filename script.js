6799acc7bd2abb6ea74c313da87adb053b8706e7
Akash Mali <71146670+akashm99@users.noreply.github.com>
2020-11-03 15:50:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked:', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'default.css'
+    }
+
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'blue.css'
+    }
+
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'green.css'
+    }
+
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
+
+$(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+
+    });
+
+        $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Student","Engineer","Developer", "Aspiring-MLE"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    
+        
+   
